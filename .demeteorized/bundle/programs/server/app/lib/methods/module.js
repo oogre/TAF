033f9cf6d47b994d2478a27bfd4907ea9a4c52f1
ogre <vincent@ogre.be>
2016-01-01 17:45:34 +0100

    moduleDestroyer

--- .demeteorized/bundle/programs/server/app/lib/methods/module.js
+++ .demeteorized/bundle/programs/server/app/lib/methods/module.js
@@ -36,7 +36,14 @@ Meteor.methods({
 			Router.go("module.index");
 		}
 		return true;
-	}
+	},
+	moduleDestroyer: function (moduleId) {
+		if (! Meteor.userId()) {
+			return new Meteor.Error("not-authorized");
+		}
+		this.unblock();
+		return Modules.remove(moduleId);
+	},
 });
 }).call(this);
 
