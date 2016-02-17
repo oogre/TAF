c0800ad77048a011022520292b8973e3e47e2cc0
ogre <vincent@ogre.be>
2016-02-17 13:50:14 +0100

    clean

--- start.sh
+++
@@ -1,8 +0,0 @@
-#export MONGO_URL='mongodb://taf:taf123@194.110.192.124:27017/TAF'
-#export PORT=58080
-export MAIL_URL='smtp://vincent@ogre.be:s7yd97lfmf@smtp.phpnet.org:8025'
-export KEY_GOOGLE='AIzaSyDuvd6-jA_ogFYeRm6-dfLF7r2W_YbA1hE'
-export ROOT_URL='http://localhost:3000/'
-export ROOT_FILE='/Users/ogre/Work/2015/Atelier du froid/TAF'
-
-meteor
