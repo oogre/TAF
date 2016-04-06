980da74bc33d8c9cc273b3900df833778bfced15
ogre <vincent@ogre.be>
2016-04-06 18:00:52 +0200

    super push good

--- .demeteorized/bundle/programs/web.cordova/app/client/templates/calendar/template.calendar.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("calendar");
-Template["calendar"] = new Template("Template.calendar", (function() {
-  var view = this;
-  return HTML.Raw('<div id="calendar"></div>');
-}));
-
-}).call(this);
